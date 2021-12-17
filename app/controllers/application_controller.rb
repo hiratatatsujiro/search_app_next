686aa3fd7a33c89c45669aeb4548d5f34afb69e0
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-12-17 09:28:59 +0900

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def set_search
+    if params[:q]&.dig(:name)
+      # squishメソッドで余分なスペースを削除する
+      squished_keywords = params[:q][:name].squish
+      ## 半角スペースを区切り文字として配列を生成し、paramsに入れる
+      params[:q][:name_cont_any] = squished_keywords.split(" ")
+    end
+    @q = Item.ransack(params[:q])
+    @items = @q.result
+  end
+end
